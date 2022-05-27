3d7ae59c350f18d71cf958c6696fbc0091b378e8
Anibal Montero <anibal.montero.p@gmail.com>
2022-05-27 22:09:48 +0200

    funciones agragar y modificar

--- script.js
+++ script.js
@@ -1,26 +1,89 @@
-var cocacola=2
-var chuleton=29
-var yogur=1
-var cobrar= {
-"mesa1": {
-    productos:[cocacola,chuleton,yogur]
-    }
+let arrayPedido = [];
+let mesactiva = '';
+
+function activarMesa(num) {
+  sessionStorage.setItem('mesaActiva', `Mesa${num}`);
+
+  if (sessionStorage.getItem(`Mesa${num}`) == null) {
+    sessionStorage.setItem(`Mesa${num}`, JSON.stringify([]));
+  }
+
+  let mesa = document.querySelector(`#mesa${num}`);
+  mesa.style.backgroundImage = "url('./img/mesactiva.jpg')";
+
+  let comanda = document.querySelector('.comanda');
+  let nodoDiv = document.createElement('div');
+  let span = document.createElement('span');
+  let info = document.createTextNode(`Mesa${num}`);
+  comanda.appendChild(nodoDiv);
+  nodoDiv.appendChild(span);
+  span.appendChild(info);
 }
 
+function comandarProducto(nombre, precio) {
+  // let mesactiva = sessionStorage.getItem('mesaActiva');
 
+  let lista_comanda = document.querySelector('#lista_comanda');
+  let nodoLi = document.createElement('li');
+  let nodoTexto = document.createTextNode(`${nombre}:${precio},`);
+  lista_comanda.appendChild(nodoLi);
+  nodoLi.appendChild(nodoTexto);
+  nodoLi.setAttribute('onclick', 'borrarProducto(this)');
+  let botonG = document.querySelector('#btn_gua');
+  botonG.style.display = 'block';
+  let botonM = document.querySelector('#btn_mod');
+  botonM.style.display = 'block';
+}
+function borrarProducto(prod) {
+  prod.parentNode.removeChild(prod);
+}
 
-function pedir(){
-    let total = cobrar.mesa1.productos.reduce(function(anterior, actual){
-        return anterior + actual;
-        });
+function guardarComanda() {
+  let lista_comanda = document.querySelector('#lista_comanda');
+  let mesactiva = sessionStorage.getItem('mesaActiva');
 
+  if (sessionStorage.getItem(mesactiva)) {
+    sessionStorage.setItem(mesactiva, JSON.stringify([]));
+  }
+  arrayPedido = JSON.parse(sessionStorage.getItem(mesactiva));
+  let pedidos = lista_comanda.textContent.split(',');
 
+  pedidos.forEach((elem) => {
+    let obj = {};
+    if (elem != '') {
+      let element = elem.split(':');
+      obj[`${element[0]}`] = element[1];
+      arrayPedido.push(obj);
+    }
+  });
 
-        var uls = document.querySelector(".lista_comanda");
-        var ul1 = uls;
-        var padre = ul1.parentNode;
-        padre.removeChild(ul1);
+  sessionStorage.setItem(mesactiva, JSON.stringify(arrayPedido));
+  lista_comanda.textContent = '';
+}
 
-   
-console.log(total)
+function cargarComanda() {
+  let mesactiva = sessionStorage.getItem('mesaActiva');
+  let arrayPedido = JSON.parse(sessionStorage.getItem(mesactiva));
+
+  if (arrayPedido.length == 0) {
+    console.log('la mesa está vacia');
+  } else {
+    let lista_comanda = document.querySelector('#lista_comanda');
+    let mesactiva = sessionStorage.getItem('mesaActiva');
+    arrayPedido = JSON.parse(sessionStorage.getItem(mesactiva));
+
+    arrayPedido.forEach((element) => {
+      let claves = Object.keys(element);
+      let valores = Object.values(element);
+      claves.forEach((elem, i) => {
+        let nodoLi = document.createElement('li');
+        let nodoTexto = document.createTextNode(`${elem}:${valores[i]},`);
+        lista_comanda.appendChild(nodoLi);
+        nodoLi.appendChild(nodoTexto);
+        nodoLi.setAttribute('onclick', 'borrarProducto(this)');
+      });
+    });
+
+    sessionStorage.setItem(mesactiva, JSON.stringify([]));
+  }
 }
